c9e8076689c9a9dcf2c1e5dcd2484cd2080b6d52
koji <kk2796@nyu.edu>
2021-07-13 14:02:18 -0400

    change color

--- src/themes/default.js
+++ src/themes/default.js
@@ -7,7 +7,8 @@ export default {
   // Colors for layout
   colors: {
     primary1: "hsl(204,23.8%,95.9%)",
-    background1: "#80D8D0",
+    // background1: "#80D8D0",
+    background1: "#0c0c0c",
     // background1: "#0F1624",
     accent1: "hsl(34.9,98.6%,72.9%)",
     button: "hsl(205.1,100%,36.1%)",
