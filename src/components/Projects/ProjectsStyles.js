9a48ae2748ff2cfb155ffbdfaf9f7240b3d9d998
koji <kk2796@nyu.edu>
2021-07-19 01:05:33 -0400

    add crypto info site to projects

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Img = styled.img`
-  width: 100%;
-  height: 100%;
+  max-width: 100%;
+  height: auto;
   object-fit: cover;
   overflow: hidden;
 `;
